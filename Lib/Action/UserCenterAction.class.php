aefd03b61571e587a3eb41839ee7083387d9d353
songxingwang <sxwnjplg@sina.com>
2011-10-19 12:00:33 +0800

    修改密码增加验证码

--- Lib/Action/UserCenterAction.class.php
+++ Lib/Action/UserCenterAction.class.php
@@ -25,10 +25,57 @@ class UserCenterAction extends Action
 	}
 
 
+	public function checkCheckNum(){
+		    $user_name = $_GET["user_name"];
+
+			$check_num = rand(100000, 999999);
+			Session::set("check_num",$check_num);
+			$sendSms = "sendSms";
+			$result = $this -> $sendSms($user_name, "您的手机验证码为".$check_num."，请及时修改密码。");
+			$json["msg"] = "验证码发送成功！";
+			$json["success"] = true;
+		    $this -> ajaxReturn($json);
+	}
+
+
+	
+	private function post($data, $target) {
+		$url_info = parse_url($target);
+		$httpheader = "POST " . $url_info['path'] . " HTTP/1.0\r\n";
+		$httpheader .= "Host:" . $url_info['host'] . "\r\n";
+		$httpheader .= "Content-Type:application/x-www-form-urlencoded\r\n";
+		$httpheader .= "Content-Length:" . strlen($data) . "\r\n";
+		$httpheader .= "Connection:close\r\n\r\n";
+		$httpheader .= $data;
+
+		$fd = fsockopen($url_info['host'], 80);
+		fwrite($fd, $httpheader);
+		$gets = "";
+		while(!feof($fd)) {
+			$gets .= fread($fd, 128);
+		}
+		fclose($fd);
+		return $gets;
+	}
+
+   private function sendSms($mobile, $content){
+		$target = "http://60.28.195.138/submitdata/service.asmx/g_Submit";
+		$post_data = "sname=dlshzy03&spwd=87654321&scorpid=&sprdid=1012818&sdst=" . $mobile . "&smsg=".rawurlencode($content);
+		$post = "post";
+		$gets = $this -> $post($post_data, $target);
+		return $gets;
+	}
+
+
+
 	public function editPassword(){
 		if(!Session::is_set("id")){
 			$this->error("没有登录！");
 		}
+		$condition["id"]  = Session::get("id");
+		$User = M("User");
+		$result = $User -> where($condition) -> find();
+		$this -> assign("result",$result);
 		$this->display();
 	}
 
@@ -36,6 +83,7 @@ class UserCenterAction extends Action
 		if(!Session::is_set("id")){
 			$this->error("没有登录！");
 		}
+		$check_num = $_REQUEST["check_num"];
 		$User = M('User');
 		$User -> create();
 		$User -> update_time = date("Y-m-d H:i:s");
@@ -44,9 +92,18 @@ class UserCenterAction extends Action
 		if(!empty($password)){
 			$User -> password = md5($password); 
 		} 
-		$User -> id = Session::get("id");
-		$User -> save();
-		$this -> ajaxReturn(1,"修改成功",2);
+		if($check_num == Session::get("check_num")){	
+			Session::set("check_num","");
+	     	$User -> id = Session::get("id");
+	     	$User -> save();
+			$json["msg"] = "修改成功！";
+			$json["success"] = true;
+		} else {
+			$json["msg"] = "验证码错误！";
+			$json["success"] = false;
+		}
+		  $this -> ajaxReturn($json);
+		
 	}
 
 
@@ -136,25 +193,6 @@ class UserCenterAction extends Action
 		}
     }
 	
-	
-	private function post($data, $target) {
-		$url_info = parse_url($target);
-		$httpheader = "POST " . $url_info['path'] . " HTTP/1.0\r\n";
-		$httpheader .= "Host:" . $url_info['host'] . "\r\n";
-		$httpheader .= "Content-Type:application/x-www-form-urlencoded\r\n";
-		$httpheader .= "Content-Length:" . strlen($data) . "\r\n";
-		$httpheader .= "Connection:close\r\n\r\n";
-		$httpheader .= $data;
-
-		$fd = fsockopen($url_info['host'], 6003);
-		fwrite($fd, $httpheader);
-		$gets = "";
-		while(!feof($fd)) {
-			$gets .= fread($fd, 128);
-		}
-		fclose($fd);
-		return $gets;
-	}
 
 	
 
--- Public/js/UserCenter/editPassword.js
+++ Public/js/UserCenter/editPassword.js
@@ -7,6 +7,43 @@ $(function(){
 			submitForm();
 		}
 	});
+
+	//检测按钮
+	$("#btnCheck").click(function(){
+		var user_name = $("#user_name").val();
+		//showWaiting();
+		$.ajax({
+			url: "checkCheckNum",
+			data: "user_name={0}".format(user_name),
+			cache: false,
+			success: function(json){
+				//alert(json);
+				json = nehnre.parseJSON(json);
+				showAlert(json.data.msg, function(){
+					if(json.data.success){
+						(function(){
+							if(!window._second && window._second != 0){
+								_second = 60;
+							}
+							if(_second > 0){
+								$("#btnCheck").val("发送验证码({0})".format(_second)).attr("disabled",true);
+								_second --;
+								setTimeout(arguments.callee,1000);
+							} else {
+								$("#btnCheck").val("再次发送").attr("disabled",false);
+							}
+						})();
+					}
+				});
+			},
+			error: function(){
+				showAlert("服务器出现错误，你的请求没有完成！");
+			},
+			complete: function(){
+			}
+		});
+	});
+
 });
 
 //检测密码是否匹配
@@ -14,10 +51,13 @@ function checkPassword(){
 	var oldpassword = $("#oldpassword").val();
 	var password = $("#password").val();
 	var passwordagain = $("#passwordagain").val();
-	if(!oldpassword){
-		showAlert("请输入原登录密码！",function(){
-			$("#oldpassword").focus();
-		});
+	var check_num = $("#check_num").val();
+	if(!check_num){
+	    showAlert("请输入验证码");
+		return false;
+	}
+	if(!/^\d{6}$/.test(check_num)){
+	    showAlert("验证码为六位数字");
 		return false;
 	}
 	if(!password){
@@ -46,35 +86,21 @@ function checkPassword(){
 
 function submitForm(){
 	var data = $("form").serialize();
-	var oldpassword = $("#oldpassword").val();
 	$.ajax({
-		url:"checkPassword",
-		type:"POST",
-		data: "oldpassword="+oldpassword,
-		success: function(json){
-           	json = nehnre.parseJSON(json);
-			if(json.data.success){
-					console.log(data);
-					$.ajax({
 						url:"updatePassword",
 						type:"POST",
 						data: data,
 						success: function(json){
-							showAlert("修改成功",function(){
-								location.href = "userCenterDetail";
+							//alert(json);
+							json = nehnre.parseJSON(json);
+							showAlert(json.data.msg, function(){
+								if(json.data.success){
+                                         location.href = "/UserCenter";
+								}
 							});
 						},
 						error: function(){
 							showAlert("提交失败，可能服务器出现故障。");
 						}
-					});
-			}else{
-				showAlert(json.data.msg);
-			}
-		},
-		error: function(){
-			showAlert("提交失败，可能服务器出现故障。");
-		}
 	});
-
 }

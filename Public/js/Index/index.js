624ba4fbaf918b5895ffec1be1c95430dfd51650
jacobi nie <nehnre@yahoo.com.cn>
2011-10-22 00:53:05 +0800

    首页js效果

--- Public/js/Index/index.js
+++ Public/js/Index/index.js
@@ -1,5 +1,11 @@
 $(function(){
-	$(".promotion .img").each(function(){
-		
+	$(".promotion_content .img div").mouseenter(function(){
+		console.log("over");
+		$(this).find("img").animate({"margin-left":-200}, 100);
+	}).mouseleave(function(){
+		console.log("out");
+		$(this).find("img").animate({"margin-left":200}, 100, function(){
+			$(this).css("margin-left", "-600px");
+		});
 	});
 });

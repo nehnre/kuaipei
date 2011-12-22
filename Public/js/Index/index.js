ff527b774fec0d55f665e97d6129a18297986e3b
songxingwang <sxwnjplg@sina.com>
2011-12-22 13:03:30 +0800

    首页滚动

--- Public/js/Index/index.js
+++ Public/js/Index/index.js
@@ -9,3 +9,30 @@ $(function(){
 		});
 	});
 });
+
+var scaleImage = function(o, w, h){ 
+	var img = new Image(); 
+	img.src = o.src; 
+	if(img.width >0 && img.height>0) { 
+		if(img.width/img.height >= w/h) 
+		{ 
+			if(img.width > w) { 
+				o.width = w; 
+				o.height = (img.height*w) / img.width; 
+			}else { 
+				o.width = img.width; 
+				o.height = img.height; 
+			} 
+				o.alt = img.width + "x" + img.height; 
+		}else { 
+			if(img.height > h) { 
+				o.height = h; 
+				o.width = (img.width * h) / img.height; 
+			} else { 
+				o.width = img.width; 
+				o.height = img.height; 
+			} 
+			o.alt = img.width + "x" + img.height; 
+		} 
+	} 
+} 
